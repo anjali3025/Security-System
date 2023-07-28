9da246ccde11df2c47f84699fc448b17d1642bfe
anjali3025 <140823699+anjali3025@users.noreply.github.com>
2023-07-28 22:35:53 +0530

    Update security_system.cpp

--- security_system.cpp
+++ security_system.cpp
@@ -2,6 +2,7 @@
 #include<fstream>
 #include<sstream>
 #include<string>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -21,7 +22,8 @@ int main()
         cin>>a;
         switch(a)
         {
-            case 1:{
+            case 1:
+            {
                 cout<<"_______________________"<<endl<<endl;
                 cout<<"|---------Register-----|"<<endl<<endl;
                 cout<<"_______________________"<<endl<<endl;
@@ -34,14 +36,16 @@ int main()
 
                 ofstream of1;
                 of1.open("file.txt");
-                if(of1.is_open()){
+                if(of1.is_open())
+                {
                     of1<<name<<"\n";
                     of1<<password0;
                     cout<<"Registration successful"<<endl;
                 }
                 break;
             }
-            case 2:{
+            case 2:
+            {
                 i=0;
                 cout<<"__________________________"<<endl<<endl;
                 cout<<"|----------Login----------|"<<endl;
@@ -57,13 +61,15 @@ int main()
                 {
                     while(!of2.eof())
                     {
-                        while(getline(of2,text)){
+                        while(getline(of2,text))
+                        {
                             istringstream iss(text);
                             iss>>word;
                             creds[i]=word;
                             i++;
                         }
-                        if(user==creds[0] && pass==creds[1]){
+                        if(user==creds[0] && pass==creds[1])
+                        {
                             cout<<"---Log in successful---";
                             cout<<endl<<endl;
                             cout<<"Details"<<endl;
@@ -71,16 +77,78 @@ int main()
                             cout<<"Password: "+ pass<<endl;
                             cout<<"Age: "+ age<<endl;
                         }
-                        else{
+                        else
+                        {
                             cout<<endl<<endl;
                             cout<<"Incorrect Credentials"<<endl;
-
-
+                            cout<<"|    1. Press 2 to login              |"<<endl;
+                            cout<<"|    2. Press 3 to change password    |"<<endl;
+                            break;
                         }
                     }
                 }
+                break;
             }
-
+            case 3:
+            {
+               i=0;
+               cout<<"--------------Change password---------------"<<endl;
+               ifstream of0;
+               of0.open("file.txt");
+               cout<<"Enter the old password:- ";
+               cin>>old;
+               if(of0.is_open())
+               {
+                while(of0.eof())
+                {
+                    while(getline(of0, text))
+                    {
+                        istringstream iss(text);
+                        iss>>word1;
+                        cp[i]=word;
+                        i++;
+                    }
+                    if(old==cp[1])
+                    {
+                        of0.close();
+                        ofstream of1;
+                        if(of1.is_open())
+                        {
+                            cout<<"Enter your new password:- ";
+                            cin>>password1;
+                            cout<<"Enter your password again:- ";
+                            cin>>password2;
+                            if(password1==password2)
+                            {
+                                of1<<cp[0]<<"\n";
+                                of1<<password1;
+                                cout<<"Password changed successfully"<<endl;
+                            }
+                            else
+                            {
+                                of1<<cp[0]<<"\n";
+                                of1<<old;
+                                cout<<"Password do not match"<<endl;
+                            }
+                        }
+                    }
+                    else{
+                        cout<<"Please enter a valid password"<<endl;
+                        break;
+                    }
+                }
+                
+               }
+               break;
+            }
+            case 4:
+            {
+                    cout<<"___________Thankyou!___________";
+                    break;
+            }
+            default:
+                cout<<"Enter a valid choice";
+            
         }
     }
     while(a!=4);
